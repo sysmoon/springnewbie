b21bccc057ea7dc4f1ef2613375d23f50661b0b7
Daniel <sysmoon@gmail.com>
2023-01-02 17:14:43 +0900

    DemoApplication annotation 원복

--- src/main/java/studio/moonid/demo/DemoApplication.java
+++ src/main/java/studio/moonid/demo/DemoApplication.java
@@ -2,15 +2,8 @@ package studio.moonid.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.GetMapping;
 
-//@SpringBootApplication(scanBasePackages = {"studio.moonid.demo"})
-//@ComponentScan(basePackages = {"studio.moonid.demo"})
-//@EntityScan(basePackages={"studio.moonid.demo.entity"})
-//@EnableJpaRepositories(basePackages={"studio.moonid.demo.repository"})
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
